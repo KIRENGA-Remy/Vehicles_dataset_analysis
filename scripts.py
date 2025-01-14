47f092dc6141213ada94da318be44735f1937824
GITOLI Remy Claudien <gitoliremy@gmail.com>
2025-01-14 08:50:02 -0700

    head, tail,info and info()

--- scripts.py
+++ scripts.py
@@ -6,9 +6,15 @@ file_path = 'vehicles.csv'
 
 try:
     data = pd.read_csv(file_path)
+    print("Load last 5 rows using tail")
+    print(data.tail())
 
-    print("Dataset successfully loaded!")
+    print("Load fist 5 rows using head")
     print(data.head())
+
+    print("Load first 5 and last 5 rows using info")
+    print(data.info) # combines head and tail functionality
+    print(data.info()) #it is an error, not transposed, it is like a table
 
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")

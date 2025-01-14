913d19af905dd3d57b15edf83627524f7f188c79
GITOLI Remy Claudien <gitoliremy@gmail.com>
2025-01-14 11:56:34 -0700

    error fix on describing both quantitative and categorical data

--- scripts.py
+++ scripts.py
@@ -1,3 +1,4 @@
+#TO RUN: jupyter notebook
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -13,7 +14,7 @@ try:
     print(data.head())
 
     print("Load first 5 and last 5 rows using info")
-    print(data.info) # combines head and tail functionality
+    print(data.info) # combines head and tail functionality, transposed
     print(data.info()) #it is an error, not transposed, it is like a table
 
     #count : return total numbers of values without null values
@@ -28,23 +29,32 @@ try:
     print("Studying nunique")
     print(data.nunique())
 
-    #describe: describes counts, mean, frequency, std:standard deviation,
+    #describe:summarize the numerical (quantitative) columns.
+    #by providing numeric summary statistics like counts, mean, frequency, std:standard deviation,...
     print("Studying describe")
     print(data.describe())
-    # but on quantitative data it dont describe these, it gives count, unique,top and frequency only
-    # print("Describe on quantitative data")
-    # sdf = data[['manufacturer','color','body_type','engine_type']]
-    # print(sdf.describe())
+    #but on categorical data it don't describe these, it gives count, unique,top and frequency only
+    #which are non-numeric summary statistics
+    print("Describe on quantitative data")
+    sdf = data[['manufacturer','color','body_type','engine_type']]
+    print(sdf.describe())
 
-    # print(sdf['manufacturer'[:10]]) # equivalence: sdf['manufacturer'].head(10)
+    print(sdf['manufacturer'][:10]) # slicing column manufacturer, equivalence: sdf['manufacturer'].head(10)
 
-    # print(sdf['manufacturer'][3:10]) #selecting from 3 to ten(10)
+    print(sdf['manufacturer'][3:10]) #selecting from 3 to ten(10)
 
     #astype: when cleaning is done before
 
     data['proposed_purchase_price'].plot.hist(bins=30, alpha=0.7,color='blue')
     plt.savefig('./purchase_price.png', format='png', dpi=400)
     plt.show()
+
+#     What Are Bins?
+# Bins are the intervals (or "buckets") into which the data values are sorted.
+# Each bin represents a range of data, and the height of the bar for that bin corresponds to the number of data points that fall within that range.
+
+# Bin Width= Range of Data / Number of Bins
+
 
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
